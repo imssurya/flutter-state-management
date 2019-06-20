d6b7fa0d374a6e0ab47f348abd1b4d825ccf8401
Udo Schöfer <developer@udos86.de>
2019-06-20 22:19:18 +0200

    provider sample refactored

--- lib/_provider/app.dart
+++ lib/_provider/app.dart
@@ -7,12 +7,15 @@ import '_state.dart';
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Provider Sample',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider(
+      builder: (context) => AppState(),
+      child: MaterialApp(
+        title: 'Provider Sample',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Page(title: 'Provider Sample'),
       ),
-      home: Page(title: 'Provider Sample'),
     );
   }
 }
@@ -24,22 +27,19 @@ class Page extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      builder: (context) => AppState(),
-      child: Scaffold(
-          appBar: AppBar(
-            title: Text(title),
-          ),
-          body: ListViewWidget(),
-          floatingActionButton: Consumer<AppState>(builder: (context, state, _widget) {
-            return FloatingActionButton(
-              onPressed: () {
-                state.addItem(Item(title: DateTime.now().toString()));
-              },
-              tooltip: 'Add',
-              child: Icon(Icons.add),
-            );
-          })),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: ListViewWidget(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          final state = Provider.of<AppState>(context, listen: false);
+          state.addItem(Item(title: DateTime.now().toString()));
+        },
+        tooltip: 'Add',
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
@@ -47,14 +47,15 @@ class Page extends StatelessWidget {
 class ListViewWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final state = Provider.of<AppState>(context);
-    return ListView.builder(
-        padding: EdgeInsets.fromLTRB(0.0, 8.0, 0.0, 8.0),
-        itemCount: state.items.length,
-        itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            title: Text(state.items[index].title),
-          );
-        });
+    return Consumer<AppState>(builder: (context, state, child) {
+      return ListView.builder(
+          padding: EdgeInsets.fromLTRB(0.0, 8.0, 0.0, 8.0),
+          itemCount: state.items.length,
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              title: Text(state.items[index].title),
+            );
+          });
+    });
   }
 }
